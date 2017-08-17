b38bf2fc3935dc9b45acd9deaf09d0293eba7c74
Steven Abreu <stevenabreu7@gmail.com>
2017-08-17 11:56:41 +0200

    + new game page

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { BoardProvider } from '../../providers/board/board';
 
 /**
  * Generated class for the GamePage page.
@@ -15,11 +16,49 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class GamePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  difficulty: number;
+  difText: string;
+  selected: number = -1;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public board: BoardProvider) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad GamePage');
+    this.difficulty = this.navParams.get('difficulty');
+    if (this.difficulty == 0) {
+      this.difText = "Easy";
+    } else if (this.difficulty == 1) {
+      this.difText = "Medium";
+    } else {
+      this.difText = "Hard";
+    }
+    this.board.createBoard(this.difficulty);
   }
 
+  solve() {
+    console.log(this.board.solve());
+  }
+
+  pressedField(row: number, col: number) {
+    this.board.makeMove(row, col, this.selected);
+    // let res = this.board.makeMove(row, col, this.selected);
+    // if (!res) {
+    //   console.log("invalid move");
+    // }
+    this.checkGame();
+  }
+
+  pressedNumber(num: number) {
+    this.selected = num;
+  }
+
+  isSelected(num: number) {
+    return (this.selected == num);
+  }
+
+  checkGame() {
+    if (this.board.isFull()) {
+      console.log("BOARD FULL.");
+    }
+  }
 }
