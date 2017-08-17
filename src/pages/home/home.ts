fa44c7278148a4611beb9a50360d32392019aab2
Steven Abreu <stevenabreu7@gmail.com>
2017-08-17 11:56:34 +0200

    + home page leads to play page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PlayPage } from '../play/play';
 
 @Component({
   selector: 'page-home',
@@ -11,4 +12,7 @@ export class HomePage {
 
   }
 
+  play() {
+    this.navCtrl.push(PlayPage);
+  }
 }
